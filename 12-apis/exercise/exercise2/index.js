931de19dfbd6d876b7e63cacc51856b4323abc57
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2017-04-05 22:13:12 -0500

    finish exercise 1

--- 12-apis/exercise/exercise2/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-var app = express()
-
-var path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(process.env.PORT || 3000);
