43817aa2176f02334b57b487ca29433747208612
Debby Chang <wanrung.chang@gmail.com>
2017-05-18 19:13:32 -0400

    assignment 02 solution

--- 02-data-types/assignment/distance.js
+++ 02-data-types/assignment/distance.js
@@ -1,8 +1,22 @@
-let x1 = 1
-let y1 = 3
-let x2 = 10
-let y2 = 15
-let deltaX = x2 - x1
-let deltaY = y2 - y1
-let distance = Math.sqrt( Math.pow( deltaX, 2 ) + Math.pow( deltaY, 2 ) )
-console.log( distance )
+// let x1 = 1
+// let y1 = 3
+// let x2 = 10
+// let y2 = 15
+// let deltaX = x2 - x1
+// let deltaY = y2 - y1
+// let distance = Math.sqrt( Math.pow( deltaX, 2 ) + Math.pow( deltaY, 2 ) )
+// console.log( distance )
+
+console.log('process argv',process.argv); //prints an array when you run node distance.js 1,3,10,15
+
+// let coord1 = process.argv[2].split(',')
+// let coord2 = process.argv[3].split(',')
+// console.log('cords', coord1, coord2)
+
+// let x1 = parseInt( coord1[0] )
+// let y1 = parseInt( coord1[1] )
+// let x2 = parseInt( coord2[0] )
+// let y2 = parseInt( coord2[1] )
+
+// console.log (x1, y1)
+// console.log( x2, y2)
