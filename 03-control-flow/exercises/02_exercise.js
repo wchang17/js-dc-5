faeacafdf4a6091cdeee2656a28ef99bd0ee1be4
Debby Chang <wanrung.chang@gmail.com>
2017-05-18 21:30:32 -0400

    week 3 exercises

--- 03-control-flow/exercises/02_exercise.js
+++ 03-control-flow/exercises/02_exercise.js
@@ -6,15 +6,54 @@
 
 // Create an array with the names 'curly', 'lary' and 'moe'
 
+let example = ['curly','lary','moe']
+
 
 /*
 Create a simple while loop that iterates over the array of names and `console.log`s them
 */
+// for (let i = 0; i < example.length; i++) {
+// 	console.log( example[i])
+// }
+
+// let x = example.length - 1
+// while ( x >= 0) {
+
+// console.log(example[x])
+
+// x--
+// }
+
 
 
 /*
-Create a simple while loop that iteratoes over each name in the array of names. Inside your loop, if the name is 'curly', then `console.log` 'curly is the best', otherwise `console.log` the current name is one of the three stooges
+Create a simple while loop that iterates over each name in the array of names. Inside your loop, if the name is 'curly', then `console.log` 'curly is the best', otherwise `console.log` the current name is one of the three stooges
 */
+
+// let x = example.length - 1
+
+// while ( x >= 0) {
+
+// if ( example[x] === "curly") {
+// 	console.log("curly is the best")
+// } else {
+// 	console.log(example[x])
+// }
+
+// x--
+// }
 
 
 // Create a `do while` loop that does the same as the above
+let x = example.length - 1
+
+do {
+if ( example[x]=== "curly") {
+
+	console.log("curly is the best")
+
+} else {
+	console.log("other")
+}
+x--
+} while( x >= 0)
--- 03-control-flow/exercises/03_exercise.js
+++ 03-control-flow/exercises/03_exercise.js
@@ -5,8 +5,23 @@
 
   Prompt:
 
-  If we list the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+  If we list the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. * Try to solve this first to check your logic.
 
   Find the sum of all multiples of 3 or 5 below 1000.
+
   
 */
+let ceiling = 10
+let total = 0
+
+for(let i =0; i < 10; i++) {
+	if( i % 3 === 0 || i % 5 === 0 ) {
+		
+		multiples.push( i )
+	} 
+}
+
+console.log( multiples )
+
+let total = 0
+for(let i =0; i < 10; i++) 
