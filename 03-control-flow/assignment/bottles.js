7782c2ad2d95d9922501d3693baf2507d60c77ba
Debby Chang <wanrung.chang@gmail.com>
2017-05-21 18:03:27 -0400

    adding bottles and fizzbuzz assignments

---
+++ 03-control-flow/assignment/bottles.js
@@ -0,0 +1,15 @@
+
+let text = " "
+
+for(let i =10; i >= 0; i--) {
+		if ( i === 0 ){
+			console.log( "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall." )
+		} else if ( i === 1 ) {
+			console.log(i + " bottle of beer on the wall," + i + " bottle of beer")
+			console.log("Take one down, pass it around. No more bottles of beer on the wall")
+		}
+		else{
+		console.log(i + " bottles of beer on the wall," + i + " bottles of beer")
+		console.log("Take one down, pass it around. " + (i-1) + " bottles of beer on the wall.")
+	}
+}
---
+++ 03-control-flow/assignment/fizzbuzz.js
@@ -0,0 +1,10 @@
+
+for(let i =0; i < 10; i++) {
+	if( i % 3 === 0 ) {
+		console.log("fizz")
+	} else if ( i % 5 === 0 ) {
+		console.log("buzz")
+	} else {
+		console.log(i)
+	}
+}
