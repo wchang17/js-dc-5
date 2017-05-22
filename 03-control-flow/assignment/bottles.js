6b0e878815829ccfb4f42e425ab9d52651ca6451
Debby Chang <wanrung.chang@gmail.com>
2017-05-22 19:42:45 -0400

    edit to 03 assignments

--- 03-control-flow/assignment/bottles.js
+++ 03-control-flow/assignment/bottles.js
@@ -1,6 +1,4 @@
 
-let text = " "
-
 for(let i =10; i >= 0; i--) {
 		if ( i === 0 ){
 			console.log( "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall." )
--- 03-control-flow/assignment/fizzbuzz.js
+++ 03-control-flow/assignment/fizzbuzz.js
@@ -1,10 +1,23 @@
 
-for(let i =0; i < 10; i++) {
-	if( i % 3 === 0 ) {
+// for(let i = 0; i < 10; i++) {
+// 	if( i % 3 === 0 ) {
+// 		console.log("fizz")
+// 	} else if ( i % 5 === 0 ) {
+// 		console.log("buzz")
+// 	} else {
+// 		console.log(i)
+// 	}
+// }
+
+let i = 0
+
+while (i < 10 ) {
+if( i % 3 === 0 ) {
 		console.log("fizz")
 	} else if ( i % 5 === 0 ) {
 		console.log("buzz")
 	} else {
 		console.log(i)
 	}
+i++
 }
