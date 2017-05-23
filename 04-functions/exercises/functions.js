220834e35541318e5d358cc602228a75463aef10
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2017-05-23 07:51:51 -0400

    uncomment exercises in function.js demo file for lesson 04

--- 04-functions/exercises/functions.js
+++ 04-functions/exercises/functions.js
@@ -166,11 +166,11 @@
 
 
 // Functions as variables
-var myFunction = function () {
-  console.log( 'myFunction variable' )
-}
-
-myFunction()
+// var myFunction = function () {
+//   console.log( 'myFunction variable' )
+// }
+// 
+// myFunction()
 
 
 /*
@@ -181,20 +181,20 @@ myFunction()
 
 
 // Pure functions
-function isCurrentYearLeapYear() {
-
-   const year = new Date().getFullYear()
-
-   if(year % 4 !== 0) {
-     return false
-   } else if(year % 100 != 0) {
-     return true
-   } else if(year % 400 != 0) {
-     return false
-   } else {
-     return true
-   }
-
-}
-
-console.log( 'is it a leap year? ', isCurrentYearLeapYear() )
+// function isCurrentYearLeapYear() {
+// 
+//    const year = new Date().getFullYear()
+// 
+//    if(year % 4 !== 0) {
+//      return false
+//    } else if(year % 100 != 0) {
+//      return true
+//    } else if(year % 400 != 0) {
+//      return false
+//    } else {
+//      return true
+//    }
+// 
+// }
+// 
+// console.log( 'is it a leap year? ', isCurrentYearLeapYear() )
