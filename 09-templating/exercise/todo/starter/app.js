13dcf0c9cd7a6699ecf6f7ebb4983918642bd442
Debby Chang <wanrung.chang@gmail.com>
2017-06-10 18:38:28 -0400

    add part 2 of lesson 9 assignment

--- 09-templating/exercise/todo/starter/app.js
+++ 09-templating/exercise/todo/starter/app.js
@@ -1,12 +1,38 @@
 let form = document.querySelector('#form')
-let submit = document.querySelector('button')
+let submit = document.querySelector('#submit')
+let delete = document.querySelector('#delete')
 let source = document.querySelector('#list')
 
-let data = {
+let DoHW = {
+	toDo : 'Do HW'
+	isComplete : false
+}
 
-	todos: ['Do HW', 'Sleep', 'Eat', 'Work']
+let Sleep = {
+	toDo : 'Sleep'
+	isComplete : false
+}
+
+let Eat = {
+	toDo : 'Sleep'
+	isComplete : false
+}
+
+class todos {
+	constructor( isComplete ) {
+
+		this.isComplete = true
 
 	}
+
+	completeTask : function() {
+		if (this.isComplete === true) {
+			console.log('Done')
+		} else {
+			console.log('Working on it')
+		}
+	}
+}
 
 submit.addEventListener('click', function(e){
 e.preventDefault()
@@ -21,3 +47,28 @@ let renderedTemplate = listTemplate( data )
 document.querySelector('#app').innerHTML = renderedTemplate
 
 })
+
+
+// let form = document.querySelector('#form')
+// let submit = document.querySelector('button')
+// let source = document.querySelector('#list')
+
+// let data = {
+
+// 	todos: ['Do HW', 'Sleep', 'Eat', 'Work']
+
+// 	}
+
+// submit.addEventListener('click', function(e){
+// e.preventDefault()
+
+// let value = document.querySelector('input').value
+// data.todos.push(value)
+// console.log(data)
+
+
+// let listTemplate = Handlebars.compile( source.innerHTML )
+// let renderedTemplate = listTemplate( data )
+// document.querySelector('#app').innerHTML = renderedTemplate
+
+// })
