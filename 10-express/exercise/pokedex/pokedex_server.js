3f5819fa622306b560a993ee9836381eb1d96d58
Debby Chang <wanrung.chang@gmail.com>
2017-06-14 22:16:12 -0400

    adding pokedex assignment

--- 10-express/exercise/pokedex/pokedex_server.js
+++ 10-express/exercise/pokedex/pokedex_server.js
@@ -3,21 +3,28 @@
 
 const express = require('express')
 const hbs = require('express-handlebars') 
+const data = require('./pokedex.json')
 const app = express()
 
+// function pokemon(id) {
+// 	for( let i = 0; i < data.pokemon.length; i++) {
+		
+// 	}
+// }
+
+app.engine('handlebars', hbs({
+	defaultLayout: 'main'
+}) ) // tells express we want to use a plugin. takes 2 params: indentifier for plugin and plugin itself. (like installing plugin)
+
+app.set('view engine', 'handlebars') //we want express to render our views for us through handlebars (like activating plugin)
+
 app.get('/', function(req, res) {
-	res.send('Hi There')
+	res.render('home', data) //passing data in the template
 })
 
-// app.engine('handlebars', hbs({
-// 	defaultLayout: 'main'
-// }) ) // tells express we want to use a plugin. takes 2 params: indentifier for plugin and plugin itself. (like installing plugin)
-
-// app.set('view engine', 'handlebars') //we want express to render our views for us through handlebars (like activating plugin)
-
-// app.get('/', function(req, res) {
-// 	res.render('home', {title: 'Test'}) //passing data in the template
-// })
+app.get('/', function(req, res) {
+	res.render('pokemon', data) //passing data in the template
+})
 
 app.listen(3000, function() {
 	console.log('listening')
