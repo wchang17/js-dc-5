399899c8533ebcf822a31f3b119686557d56ab86
Debby Chang <wanrung.chang@gmail.com>
2017-06-29 21:15:25 -0400

    adding lesson 15 exercise

--- 15-authentication/exercise/authentication/app.js
+++ 15-authentication/exercise/authentication/app.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose")
 const hbs = require('express-handlebars')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 
 const routes = require('./routes/index')
 
 const app = express()
-mongoose.connect("mongodb://localhost:27017/peoplebook")
+mongoose.connect('mongodb://admin:admin@pokedex-shard-00-00-emqy4.mongodb.net:27017,pokedex-shard-00-01-emqy4.mongodb.net:27017,pokedex-shard-00-02-emqy4.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=pokedex-shard-0&authSource=admin')
 
 app.engine('handlebars', hbs({
   defaultLayout: 'default',
@@ -28,6 +31,20 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(session({
+  secret: 'TaylorSwiftFan2017',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+//configure passport
+const User = require('./models/user') //import our account model
+passport.use(new LocalStrategy(User.authenticate())) //tell passport to use a local strategy with our user model
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 
 app.use('/', routes)
 
--- 15-authentication/exercise/authentication/routes/index.js
+++ 15-authentication/exercise/authentication/routes/index.js
@@ -1,4 +1,7 @@
 const express = require("express")
+const passport = require('passport')
+
+const User = require('../models/user')
 
 // Create a router for handling our application as
 // well as our sign-up flow
@@ -7,8 +10,42 @@ const router = express.Router()
 // Index route
 router.get("/", function( req, res ) {
 
-  res.render('index')
+  res.render('index', {user: req.user})
 
+})
+
+router.get("/signup", function(req, res) {
+	res.render('signup', {})
+})
+
+router.post("/signup", function(req,res) {
+	User.register(new User({
+		username: req.body.username
+	}), 
+	req.body.password,
+	(err, account) => {
+		if(err) {
+			return res.render('register', {account: account})
+		}
+		console.log(account)
+		passport.authenticate('local')(req, res, () =>{
+			res.redirect('/')
+		})
+	} 
+	)
+})
+
+router.get('/login', (req, res) =>{
+	res.render('login', {user: req.user})
+})
+
+router.post('/login', passport.authenticate('local'), (req,res) =>{
+	res.redirect('/')
+})
+
+router.get('/logout', (req, res) =>{
+	req.logout()
+	res.redirect('/')
 })
 
 module.exports = router
