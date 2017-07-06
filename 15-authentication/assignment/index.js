1c8042c02c01678478f918ea9d0ea4f4b35a89fd
Debby Chang <wanrung.chang@gmail.com>
2017-07-06 13:00:36 -0400

    add lesson 15 assignment

--- 15-authentication/assignment/index.js
+++ 15-authentication/assignment/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const session = require('expression-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 
 mongoose.connect('mongodb://jsdc5:taylorswift2017@ds135912.mlab.com:35912/jsdc5tunr')
 
@@ -11,10 +15,38 @@ const artistController = require('./routes/artists.js')
 
 const Artist = require('./models/Artist.js')
 
-app.engine('handlebars', hbs({defaultLayout: 'main'}))
+app.engine('handlebars', hbs({
+	defaultLayout: 'main',
+	helpers: {
+		equal: function(lvalue, rvalue, options) {
+			if (arguments.length < 3)
+            throw new Error("Handlebars Helper equal needs 2 parameters")
+        if( lvalue!=rvalue ) {
+            return options.inverse(this)
+        } else {
+            return options.fn(this)
+        }
+		}
+	}
+}))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(session({
+  secret: 'TaylorSwiftFan2017',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+//configure passport
+const User = require('./models/user') //import our account model
+passport.use(new LocalStrategy(User.authenticate())) //tell passport to use a local strategy with our user model
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.use( express.static('public') )
 
--- 15-authentication/assignment/routes/index.js
+++ 15-authentication/assignment/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const passport = require('passport')
 
 const appController = express.Router()
 
+const User = require('../models/user')
 const Artist = require('../models/Artist.js')
 
 appController.get('/', ( req, res ) => {
@@ -12,6 +14,40 @@ appController.get('/', ( req, res ) => {
 
 appController.get('/about', ( req, res ) => {
   res.render('about')
+})
+
+router.get("/signup", function(req, res) {
+	res.render('signup', {})
+})
+
+router.post("/signup", function(req,res) {
+	User.register(new User({
+		username: req.body.username
+	}), 
+	req.body.password,
+	(err, account) => {
+		if(err) {
+			return res.render('register', {account: account})
+		}
+		console.log(account)
+		passport.authenticate('local')(req, res, () =>{
+			res.redirect('/')
+		})
+	} 
+	)
+})
+
+router.get('/login', (req, res) =>{
+	res.render('login', {user: req.user})
+})
+
+router.post('/login', passport.authenticate('local'), (req,res) =>{
+	res.redirect('/')
+})
+
+router.get('/logout', (req, res) =>{
+	req.logout()
+	res.redirect('/')
 })
 
 module.exports = appController
